70ec42bf99532cb978323a073c7e54025f798fa0
cheetahtec <abhinav@cheetah.tech>
2024-03-01 12:40:13 +0530

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,22 +5,24 @@ import {
   Image,
   Spinner,
   Button,
+  Heading,
 } from '@chakra-ui/react';
 
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
 interface Prop {
-  handleSelectGenre : (genre: Genre) => void;
-  selectedGenre: Genre | null
+  handleSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({selectedGenre,handleSelectGenre}:Prop) => {
+const GenreList = ({ selectedGenre, handleSelectGenre }: Prop) => {
   const { data, error, isLoading } = useGenres();
   if (isLoading) return <Spinner></Spinner>;
   if (error) return null;
   return (
     <>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
       {data && (
         <List>
           {data.map((genre) => (
@@ -29,12 +31,15 @@ const GenreList = ({selectedGenre,handleSelectGenre}:Prop) => {
                 <Image
                   boxSize="32px"
                   borderRadius={8}
+                  objectFit='cover'
                   src={getCroppedImageUrl(genre.image_background)}
                 ></Image>
                 <Button
                   fontSize="lg"
                   variant="link"
-                  fontWeight={selectedGenre?.id === genre.id?'bold':''}
+                  whiteSpace="normal"
+                  textAlign="left"
+                  fontWeight={selectedGenre?.id === genre.id ? 'bold' : ''}
                   onClick={() => handleSelectGenre(genre)}
                 >
                   {genre.name}
